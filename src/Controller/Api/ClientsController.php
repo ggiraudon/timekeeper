9ad4ad8ba58ae4fe663ce9e3b32540093cd8621e
Guillaume Giraudon <ggiraudon@baselinetelematics.com>
2017-12-20 17:40:57 -0500

    Added client mods

--- src/Controller/Api/ClientsController.php
+++ src/Controller/Api/ClientsController.php
@@ -6,4 +6,27 @@ use Cake\Event\Event;
 
 class ClientsController extends AppController
 {
+	public function initialize()
+        {
+                parent::initialize();
+                $this->Auth->allow(['webhook']);
+        }
+
+	public function webhook() {
+		//$this->autoRender = false;
+		file_put_contents("/tmp/actions.log",print_r($_REQUEST,true)."\n",FILE_APPEND);
+		file_put_contents("/tmp/actions.log",print_r($this->request->data,true)."\n",FILE_APPEND);
+		file_put_contents("/tmp/actions.log",json_encode($this->request->data)."\n",FILE_APPEND);
+
+		$response = [
+				"speech" => "i like to move it move it",
+				"displayText" => "i like to move it move it",
+				//"contextOut" => [],
+				//"source" => "",
+				//"followupEvent" => []
+			    ];
+
+		$this->set('response',$response);
+		$this->set('_serialize','response');
+	}
 }
